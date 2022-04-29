342e405eb390b4923d5156f3da90e6764e025a48
klina8575 <klina8575@gmail.com>
2022-04-29 15:39:56 +0900

    up

---
+++ src/components/Orders.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Orders() {
+  return (
+    <aside>
+      <div className="order">
+        <div className="body">body</div>
+        <div className="total">total</div>
+      </div>
+    </aside>
+  );
+}
+
+export default Orders;
---
+++ src/components/Prototypes.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Prototypes() {
+  return (
+    <main>
+      <div className="prototypes">Prototypes</div>
+    </main>
+  );
+}
+
+export default Prototypes;
