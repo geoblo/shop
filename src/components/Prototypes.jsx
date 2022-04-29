3ff2621d24e468b971e24aebd460197ccc6e8b88
klina8575 <klina8575@gmail.com>
2022-04-29 16:51:20 +0900

    up

--- src/components/Prototypes.jsx
+++ src/components/Prototypes.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import useActions from "../hooks/useAction";
+import usePrototypes from "./../hooks/usePrototypes";
 
 function Prototypes() {
+  const prototypes = usePrototypes();
+  console.log(prototypes);
   return (
     <main>
-      <div className="prototypes">Prototypes</div>
+      <div className="prototypes">
+        {prototypes.map((prototype) => {
+          const { id, thumbnail, title, price, desc, pieUrl } = prototype;
+          return (
+            <div className="prototype" key={id}>
+              <a href={pieUrl} target="_blank" rel="noreferrer">
+                <div>
+                  <video
+                    autoPlay
+                    loop
+                    playsInline
+                    className="prototype__artwork prototype__edit"
+                    src={thumbnail}
+                    style={{
+                      objectFit: "contain",
+                    }}
+                  />
+                </div>
+              </a>
+              <div className="prototype__body">
+                <div className="prototype__title"></div>
+                <p className="prototype__price"></p>
+                <p className="prototype__desc"></p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
     </main>
   );
 }
