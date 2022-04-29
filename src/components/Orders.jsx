13810705421b549f9c6981f8147f236bf28579da
klina8575 <klina8575@gmail.com>
2022-04-29 17:55:44 +0900

    up

--- src/components/Orders.jsx
+++ src/components/Orders.jsx
@@ -1,10 +1,55 @@
 import React from "react";
+import { useMemo } from "react";
+import useActions from "../hooks/useAction";
+import useOrders from "./../hooks/useOrders";
+import usePrototypes from "../hooks/usePrototypes";
 
 function Orders() {
+  const orders = useOrders();
+  const prototypes = usePrototypes();
+  const { remove, removeAll } = useActions();
+
+  if (orders.length === 0) {
+    return (
+      <aside>
+        <div className="empty">
+          <div className="title">You don't have any orders</div>
+          <div className="subtitle">Click on a + to add an order</div>
+        </div>
+      </aside>
+    );
+  }
+
   return (
     <aside>
       <div className="order">
-        <div className="body">body</div>
+        <div className="body">
+          {orders.map((order) => {
+            const { id } = order;
+            const prototype = prototypes.find((p) => p.id === id);
+            const click = () => {
+              remove(id);
+            };
+            return (
+              <div className="item" key={id}>
+                <div className="img">
+                  <video src={prototype.thumbnail} />
+                </div>
+                <div className="content">
+                  <p className="title">
+                    {prototype.title} x {order.quantity}
+                  </p>
+                </div>
+                <div className="action">
+                  <p className="price">$ {prototype.price * order.quantity}</p>
+                  <button className="btn btn--link" onClick={click}>
+                    <i className="icon icon--cross" />
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
         <div className="total">total</div>
       </div>
     </aside>
